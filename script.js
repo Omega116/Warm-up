fbecb6fd2c1f21f965c03fbdf228f90b3d978991
omar <ineomine116@gmail.com>
2024-10-28 14:15:03 +0100

    task 4 completed warm up 1

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 "use strict";
 
-const filter_array_values = (arr) => {
-  return arr.filter((el) => {
-    if (el) {
-      return el;
-    }
-  });
+const date_diff_indays = (date1, date2) => {
+  return (new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24);
 };
 
-console.log(filter_array_values([58, "", "abcd", true, null, false, 0]));
+console.log(date_diff_indays("04/02/2014", "11/04/2014"));
+console.log(date_diff_indays("12/02/2014", "11/04/2014"));
