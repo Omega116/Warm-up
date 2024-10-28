6cf7063de2d3b84d2251e7c63ad49ebeee5eee9e
omar <ineomine116@gmail.com>
2024-10-28 16:29:34 +0100

    task 15 still not completed warm up 1

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 "use strict";
+const users = [
+  { name: "fred", language: "Javascript" },
+  { name: "barney", language: "TypeScript" },
+  { name: "frannie", language: "Javascript" },
+  { name: "anna", language: "Java" },
+  { name: "jimmy" },
+  { name: "nicky", language: "Python" },
+];
 
-const fact = (n) => {
-  if (n === 1) {
-    return 1;
-  } else {
-    return n * fact(n - 1);
+const orderWith = (arr, prop, order) => {
+  let final = [];
+  let semiFinal = [];
+  let c = 0;
+  while (c < order.length) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][prop] === order[c]) {
+        final.push(arr[i]);
+      }
+    }
+    c++;
   }
+
+  return final;
 };
 
-console.log(fact(6));
+console.log(orderWith(users, "language", ["Javascript", "TypeScript", "Java"]));
