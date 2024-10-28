6554b20760866cb5df1e6e3e8122fa268ba58554
omar <ineomine116@gmail.com>
2024-10-28 15:31:19 +0100

    task 7 completed warm up 1

--- script.js
+++ script.js
@@ -6,13 +6,28 @@ const showDay = (date) => {
     Number(date.slice(3, 5)) - 1,
     Number(date.slice(0, 2))
   );
-  if (Number(date.slice(3, 5)) - 1 > 12 || Number(date.slice(0, 2)) > 30) {
-    return "not a valid date";
-  } else {
-    return ourDate.toLocaleDateString("en-us", { weekday: "long" });
+  let c = 0;
+  while (ourDate.toLocaleString("en-us", { weekday: "long" }) != "Monday") {
+    ourDate = new Date(
+      Number(date.slice(-4)),
+      Number(date.slice(3, 5)) - 1,
+      Number(date.slice(0, 2)) - c
+    );
+    c++;
   }
+  /* let j = 0;
+  let nextDate = ourDate;
+  while (ourDate.toLocaleString("en-us", { weekday: "long" }) != "Sunday") {
+    nextDate = new Date(
+      Number(date.slice(-4)),
+      Number(date.slice(3, 5)) - 1,
+      Number(date.slice(0, 2)) + j
+    );
+    j++;
+  } */
+  console.log(ourDate);
+  console.log(new Date(ourDate.setDate(ourDate.getDate() + 6)));
 };
 
-console.log(showDay("07/11/2000"));
-console.log(showDay("11/06/2017"));
-console.log(showDay("11/26/2017"));
+console.log(showDay("05/12/2022"));
+console.log(showDay("30/12/2022"));
