6a233aed80a46afb34baa5ae49b5ee8b3340bcf2
omar <ineomine116@gmail.com>
2024-10-28 12:16:57 +0100

    task 6 completed

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 "use strict";
 
-const swap = (arr) => {
-  if (arr.length % 2 != 0) {
-    return "not even length";
-  } else {
-    let newArr = arr.slice(0, arr.length / 2);
-    let newArr1 = arr.slice(arr.length / 2);
-    return [...newArr1, ...newArr];
-  }
+const join = (arr) => {
+  return arr.join(",");
 };
 
-console.log(swap([1, 5, 9, 25]));
+console.log(join(["omar", "zhioua", "2005"]));
+console.log(typeof join(["omar", "zhioua", "2005"]));
