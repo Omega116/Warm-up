8ed6dd950a66bb1a01d03aac5cb66034b39386d4
omar <ineomine116@gmail.com>
2024-10-28 15:39:57 +0100

    task 8 completed warm up 1

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
 "use strict";
 
-const showDay = (date) => {
-  let ourDate = new Date(
-    Number(date.slice(-4)),
-    Number(date.slice(3, 5)) - 1,
-    Number(date.slice(0, 2))
-  );
-  let c = 0;
-  while (ourDate.toLocaleString("en-us", { weekday: "long" }) != "Monday") {
-    ourDate = new Date(
-      Number(date.slice(-4)),
-      Number(date.slice(3, 5)) - 1,
-      Number(date.slice(0, 2)) - c
-    );
-    c++;
+const swapcase = (str) => {
+  let newStr = "";
+  for (const letter of str) {
+    if (letter === letter.toUpperCase()) {
+      newStr += letter.toLowerCase();
+    } else {
+      newStr += letter.toUpperCase();
+    }
   }
-  /* let j = 0;
-  let nextDate = ourDate;
-  while (ourDate.toLocaleString("en-us", { weekday: "long" }) != "Sunday") {
-    nextDate = new Date(
-      Number(date.slice(-4)),
-      Number(date.slice(3, 5)) - 1,
-      Number(date.slice(0, 2)) + j
-    );
-    j++;
-  } */
-  console.log(ourDate);
-  console.log(new Date(ourDate.setDate(ourDate.getDate() + 6)));
+  return newStr;
 };
-
-console.log(showDay("05/12/2022"));
-console.log(showDay("30/12/2022"));
+console.log(swapcase("AaBbc"));
