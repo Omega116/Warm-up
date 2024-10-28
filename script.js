8114c7b7839df21cf610d81f70143f9ddc2856a9
omar <ineomine116@gmail.com>
2024-10-28 13:26:37 +0100

    realtask8 completed

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 "use strict";
 
-var student = {
-  name: "David Rayy",
-  sclass: "VI",
-  rollno: 12,
+const FizzBuzz = (n) => {
+  let arr = [];
+  if (n === "Fizz") {
+    let multiply = 3;
+    let counter = 1;
+    while (counter * multiply <= 100) {
+      arr.push(counter * multiply);
+      counter++;
+    }
+  } else if (n === "Buzz") {
+    let multiply = 5;
+    let counter = 1;
+    while (counter * multiply <= 100) {
+      arr.push(counter * multiply);
+      counter++;
+    }
+  } else if (n === "FizzBuzz") {
+    for (let i = 3; i < 100; i++) {
+      if (i % 3 === 0) {
+        arr.push(i);
+      }
+      if (i % 5 === 0) {
+        arr.push(i);
+      }
+    }
+  } else {
+    arr = Array.from({ length: 100 }, (_, i) => i + 1);
+  }
+  for (const item of arr) {
+    console.log(item);
+  }
 };
 
-const removeProperty = (obj, prop) => {
-  delete obj[prop];
-};
-
-console.log(student);
-removeProperty(student, "rollno");
-console.log(student);
+console.log(FizzBuzz("FizzBuzz"));
