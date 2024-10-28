24ca17a2b7ef2d1191cbe490c46810b990e0a738
omar <ineomine116@gmail.com>
2024-10-28 12:14:08 +0100

    task 5 completed

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const replace = (str) => {
-  let newStr = str.replace(/\d/, "$");
-  if (newStr.includes("$")) {
-    return newStr;
+const swap = (arr) => {
+  if (arr.length % 2 != 0) {
+    return "not even length";
   } else {
-    return "no digits";
+    let newArr = arr.slice(0, arr.length / 2);
+    let newArr1 = arr.slice(arr.length / 2);
+    return [...newArr1, ...newArr];
   }
 };
 
-console.log(replace("sdfv6v0"));
+console.log(swap([1, 5, 9, 25]));
