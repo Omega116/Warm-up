17411083691d5f8c56e49a2cf6a1faeb290ece53
omar <ineomine116@gmail.com>
2024-10-28 13:29:39 +0100

    task 9 completed

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
 "use strict";
 
-const FizzBuzz = (n) => {
-  let arr = [];
-  if (n === "Fizz") {
-    let multiply = 3;
-    let counter = 1;
-    while (counter * multiply <= 100) {
-      arr.push(counter * multiply);
-      counter++;
-    }
-  } else if (n === "Buzz") {
-    let multiply = 5;
-    let counter = 1;
-    while (counter * multiply <= 100) {
-      arr.push(counter * multiply);
-      counter++;
-    }
-  } else if (n === "FizzBuzz") {
-    for (let i = 3; i < 100; i++) {
-      if (i % 3 === 0) {
-        arr.push(i);
-      }
-      if (i % 5 === 0) {
-        arr.push(i);
-      }
-    }
-  } else {
-    arr = Array.from({ length: 100 }, (_, i) => i + 1);
-  }
-  for (const item of arr) {
-    console.log(item);
+const pattern = (n, el) => {
+  for (let i = 1; i <= n; i++) {
+    console.log(el.repeat(i));
   }
 };
 
-console.log(FizzBuzz("FizzBuzz"));
+pattern(5, "*");
