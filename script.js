4df10cff2131103c279b27bbdd379fa780de4353
omar <ineomine116@gmail.com>
2024-10-28 14:26:50 +0100

    task 5 completed warm up 1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const date_diff_indays = (date1, date2) => {
-  return (new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24);
+const days_passed = (date) => {
+  return (date - new Date(date.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24);
 };
 
-console.log(date_diff_indays("04/02/2014", "11/04/2014"));
-console.log(date_diff_indays("12/02/2014", "11/04/2014"));
+console.log(days_passed(new Date(2015, 0, 15)));
+
+console.log(days_passed(new Date(2015, 11, 14)));
