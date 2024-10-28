b36005a3049eb49abe2e20904bc886efff4d393d
omar <ineomine116@gmail.com>
2024-10-28 15:02:13 +0100

    task 6 completed warm up 1

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 "use strict";
 
-const days_passed = (date) => {
-  return (date - new Date(date.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24);
+const showDay = (date) => {
+  let ourDate = new Date(
+    Number(date.slice(-4)),
+    Number(date.slice(3, 5)) - 1,
+    Number(date.slice(0, 2))
+  );
+  if (Number(date.slice(3, 5)) - 1 > 12 || Number(date.slice(0, 2)) > 30) {
+    return "not a valid date";
+  } else {
+    return ourDate.toLocaleDateString("en-us", { weekday: "long" });
+  }
 };
 
-console.log(days_passed(new Date(2015, 0, 15)));
-
-console.log(days_passed(new Date(2015, 11, 14)));
+console.log(showDay("07/11/2000"));
+console.log(showDay("11/06/2017"));
+console.log(showDay("11/26/2017"));
